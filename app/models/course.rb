0c140c95fc557fd583b2fff7428b0c9b7ce52bd9
Ricardo Piro-Rael <fdisk@fdisk.co>
2015-06-24 23:34:56 -0600

    Working tree methods

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,13 @@
 class Course < Requirement
+  validates :min_grade, absence: true
+  validates :take, absence: true
+  # This is the base case and should return self
+  def subtree
+    self
+  end
+
+  def named_subtree
+    { name: name }
+  end
+
 end
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,2 +1,20 @@
 class Requirement < ActiveRecord::Base
+  has_many :parent_edges, class_name: 'Edge', foreign_key: 'child_id'
+  has_many :child_edges, class_name: 'Edge', foreign_key: 'parent_id'
+  has_many :children, through: :child_edges
+  has_many :parents, through: :parent_edges
+  validates :name, presence: true
+
+  def subtree
+    { self => children.map(&:subtree) }
+  end
+
+  def named_subtree(width = nil)
+    width ||= [children.count { |child| child.is_a? Requirement },10].max
+    { name: name,
+      children: (children.sample(width).map(&:named_subtree) << (
+        { name: "+#{children.size - width } more" } if children.size > width)).compact 
+    }
+  end
+
 end
