1bb7031f8f0110b708ca269b16c07730c226744d
Ricardo Piro-Rael <fdisk@fdisk.co>
2015-07-02 19:45:32 -0600

    Working reasoner

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,9 @@
 class Course < Requirement
   validates :min_grade, absence: true
   validates :take, absence: true
+
+  attr_accessor :grade
+
   # This is the base case and should return self
   def subtree
     self
@@ -8,6 +11,19 @@ class Course < Requirement
 
   def named_subtree
     { name: name }
+  end
+
+  def satisfy(student_set, minimum_grade)
+    course = student_set.find { |course| course.name == name }
+    @satisfied = course && course.grade >= grade_to_i(minimum_grade)
+    self
+  end
+
+  def grade_to_i(grade)
+    grade_hash = Hash.new(0)
+    grade_hash.merge(
+      ["CR", "D-", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+" ].map.with_index(1) { |grade, integer| { grade => integer } }.reduce(:merge))
+    grade_hash[grade]
   end
 
 end
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -5,6 +5,8 @@ class Requirement < ActiveRecord::Base
   has_many :parents, through: :parent_edges
   validates :name, presence: true
 
+  attr_accessor :satisfied, :uplus_children
+
   def subtree
     { self => children.map(&:subtree) }
   end
@@ -17,23 +19,27 @@ class Requirement < ActiveRecord::Base
     }
   end
 
-  def satisfy(student_set)
-    if children.map(&:children).flatten.all? { |item| item.is_a? Course }
-    end
+  def satisfy(student_set, minimum_grade = 0)
+    minimum_subs = take || children.size
+    subreq = children.map{ |child| child.satisfy(student_set, min_grade) }.select(&:satisfied)
+    subhours = subreq.reduce(Requirement.new(hours: 0), :uplus)
+    subhours.satisfied = subreq.size >= minimum_subs.to_i && subhours.hours >= hours.to_i
+    subhours.hours = hours
+    subhours
   end
 
   def uplus(requirement)
-    as, bs = uplus_child, requirement.uplus_child
+    as, bs = uplus_children, requirement.uplus_children
     c =  (as & bs).map(&:hours).reduce(:+).to_i
     a = [(as - bs).map(&:hours).reduce(:+).to_i, hours].min
     b = [(bs - as).map(&:hours).reduce(:+).to_i, hours].min
-    result = Requirement.new(hours: [a + b + c, hours + requirement.hours].min)
-    result.uplus_child(as & bs)
+    result = Requirement.new(hours: [a + b + c, hours.to_i + requirement.hours.to_i].min)
+    result.uplus_children = as + bs
     result
   end
 
-  def uplus_child(x = nil)
-    (@uplus_child ||= x) || children
+  def uplus_children
+    @uplus_children || children
   end
 
 end
